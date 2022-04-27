28e175afadcd732a60d2dc0c7cdeb4f1b13d717a
Piotr Przybys <piotrek.przybys@gmail.com>
2022-04-27 09:46:30 +0200

    Removed commented test body in ProductRespositoryTest

--- products/src/test/java/com/github/rush/products/service/rpc/ProductRepositoryTest.java
+++ products/src/test/java/com/github/rush/products/service/rpc/ProductRepositoryTest.java
@@ -27,44 +27,6 @@ class ProductRepositoryTest {
     @DisplayName("- should return product")
     @Test
     public void shouldReturnProduct() {
-//        ProductEntity product1 = ProductEntity.builder()
-//                .id(PRODUCT_ID)
-//                .name("name 1")
-//                .text("text 1")
-//                .aliases(
-//                        List.of(
-//                                "alias1", "alias2"
-//                        )
-//                )
-//                .categories(
-//                        List.of(
-//                                "category11", "category12"
-//                        )
-//                )
-//                .build();
-//
-//        ProductEntity product2 = ProductEntity.builder()
-//                .id(2L)
-//                .name("name 2")
-//                .text("text 2")
-//                .aliases(
-//                        List.of(
-//                                "alias2", "alias2"
-//                        )
-//                )
-//                .categories(
-//                        List.of(
-//                                "category21", "category22"
-//                        )
-//                )
-//                .build();
-//
-//        entityManager.persist(product1);
-//        entityManager.persist(product2);
-//
-//        ProductEntity product = classUnderTest.findProduct(PRODUCT_ID);
-//
-//        assertThat(product).isEqualTo(product1);
 
     }
 
