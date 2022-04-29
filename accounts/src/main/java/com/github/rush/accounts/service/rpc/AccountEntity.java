0934966ed0298c3e5a572ff83cbbb7551f3b9902
Adrian Wiśniewski <ardianows@gmail.com>
2022-04-29 02:51:41 +0200

    Account Entity added.

---
+++ accounts/src/main/java/com/github/rush/accounts/service/rpc/AccountEntity.java
@@ -0,0 +1,25 @@
+package com.github.rush.accounts.service.rpc;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@ToString
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+}
