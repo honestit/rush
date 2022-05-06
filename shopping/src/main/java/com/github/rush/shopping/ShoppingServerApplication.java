26d56919fc67b543b58bfaee11b41912f8a57e4f
honestit <michal.kupisinski@gmail.com>
2022-05-06 12:46:07 +0200

    Try to add client side load balancing

--- shopping/src/main/java/com/github/rush/shopping/ShoppingServerApplication.java
+++ shopping/src/main/java/com/github/rush/shopping/ShoppingServerApplication.java
@@ -5,17 +5,19 @@ import com.github.rush.services.ordering.GetOrderedShoppingListResponse;
 import com.github.rush.services.ordering.OrderingServiceGrpc;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @Slf4j
 public class ShoppingServerApplication implements CommandLineRunner {
 
-  @GrpcClient("ordering")
+  @GrpcClient("ordering") @LoadBalanced
   private OrderingServiceGrpc.OrderingServiceBlockingStub orderingClient;
 
   public static void main(String[] args) {
