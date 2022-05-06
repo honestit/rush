799eb2a85dfc7bdc066e8a387df11a5a52849f12
honestit <michal.kupisinski@gmail.com>
2022-05-06 12:25:19 +0200

    Use ordering client inside shopping service module

---
+++ shopping/src/main/java/com/github/rush/shopping/ServiceClientConfiguration.java
@@ -0,0 +1,10 @@
+package com.github.rush.shopping;
+
+import com.github.rush.services.ordering.OrderingServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceClientConfiguration {
+
+}
